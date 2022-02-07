36e45554f225de8d420026fb8ee18fd620648e24
justinbanaga <justinbanaga.195@gmail.com>
2022-02-07 11:26:11 +0800

    fix errors display

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     if @article.update(article_params)
-      redirect @article
+      redirect_to @article
     else
       render :edit, status: :unprocessable_entity
     end
